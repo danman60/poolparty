b05886cbe21f68c7a2af6eb3934feb53bc67dfd8
danman60 <danman60@users.noreply.github.com>
2025-10-17 23:11:50 -0400

    abi: add increaseLiquidity to NonfungiblePositionManager ABI for Add Liquidity action

--- src/lib/abis/NonfungiblePositionManager.ts
+++ src/lib/abis/NonfungiblePositionManager.ts
@@ -35,6 +35,31 @@ export const NONFUNGIBLE_POSITION_MANAGER_ABI = [
       {
         "components": [
           { "internalType": "uint256", "name": "tokenId", "type": "uint256" },
+          { "internalType": "uint256", "name": "amount0Desired", "type": "uint256" },
+          { "internalType": "uint256", "name": "amount1Desired", "type": "uint256" },
+          { "internalType": "uint256", "name": "amount0Min", "type": "uint256" },
+          { "internalType": "uint256", "name": "amount1Min", "type": "uint256" },
+          { "internalType": "uint256", "name": "deadline", "type": "uint256" }
+        ],
+        "internalType": "struct INonfungiblePositionManager.IncreaseLiquidityParams",
+        "name": "params",
+        "type": "tuple"
+      }
+    ],
+    "name": "increaseLiquidity",
+    "outputs": [
+      { "internalType": "uint128", "name": "liquidity", "type": "uint128" },
+      { "internalType": "uint256", "name": "amount0", "type": "uint256" },
+      { "internalType": "uint256", "name": "amount1", "type": "uint256" }
+    ],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "components": [
+          { "internalType": "uint256", "name": "tokenId", "type": "uint256" },
           { "internalType": "uint128", "name": "liquidity", "type": "uint128" },
           { "internalType": "uint256", "name": "amount0Min", "type": "uint256" },
           { "internalType": "uint256", "name": "amount1Min", "type": "uint256" },
